c54b637ac13e842c1206666dfb83dcf9294026a9
David Enke <david@enke.dev>
2024-10-26 02:10:42 +0200

    test: all list variants (field, fields and types)

---
+++ src/transformers/field-list.transform.spec.ts
@@ -0,0 +1,50 @@
+import type { CmsField, CmsFieldBase, CmsFieldList } from 'decap-cms-core';
+import z from 'zod';
+
+import { transformListField } from './field-list.transform.js';
+
+describe('field-list.transform', () => {
+  it('allows parsing with field based schema', () => {
+    const field = {
+      name: 'foo',
+      widget: 'list',
+      field: { name: 'foo', widget: 'text' } as CmsField,
+    } as CmsFieldBase & CmsFieldList;
+    const { runtime } = transformListField(field, z);
+    const result = ['foo', 'bar', 'baz'];
+    expect(() => runtime.parse(result)).not.toThrow();
+  });
+
+  it('allows parsing with fields based schema', () => {
+    const field = {
+      name: 'foo',
+      widget: 'list',
+      fields: [
+        { name: 'foo', widget: 'text' } as CmsField,
+        { name: 'bar', widget: 'number' } as CmsField,
+      ],
+    } as CmsFieldBase & CmsFieldList;
+    const { runtime } = transformListField(field, z);
+    const result = [{ foo: 'foo', bar: 123 }];
+    expect(() => runtime.parse(result)).not.toThrow();
+  });
+
+  it('allows parsing with type based schema', () => {
+    const field = {
+      name: 'foo',
+      widget: 'list',
+      types: [
+        {
+          name: 'foo',
+          fields: [
+            { name: 'foo', widget: 'text' } as CmsField,
+            { name: 'bar', widget: 'number' } as CmsField,
+          ],
+        },
+      ],
+    } as CmsFieldBase & CmsFieldList;
+    const { runtime } = transformListField(field, z);
+    const result = [{ type: 'foo', foo: 'foo', bar: 123 }];
+    expect(() => runtime.parse(result)).not.toThrow();
+  });
+});
