6f6c621cc6e6ff95ec2ce17c9d5ac7f6fc18d246
Fermain <Fermain@users.noreply.github.com>
2024-12-12 12:23:02 +0200

    Removes required handling

--- src/transformers/field-relation.transform.ts
+++ src/transformers/field-relation.transform.ts
@@ -4,11 +4,10 @@ import type { ZodTypeAny } from 'zod';
 import type { Transformer } from '../utils/transform.utils.js';
 
 export const transformRelationField: Transformer<ZodTypeAny, CmsFieldBase & CmsFieldRelation> = (
-  { collection, multiple = false, required = true },
+  { collection, multiple = false },
   z,
 ) => {
-  const base = multiple ? z.array(z.string()) : z.string();
-  const runtime = required ? base : base.optional();
+  const runtime = multiple ? z.array(z.string()) : z.string();
 
   const cptime = multiple ? `z.array(reference('${collection}'))` : `reference('${collection}')`;
 
