c6d63392fb70968772228bb79e5ed7bb1b03b07f
David Enke <david@enke.dev>
2024-12-14 15:57:54 +0100

    fix(demo): inline svgs and align color

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
-import { defineConfig } from 'vite';
+import { readFileSync } from 'node:fs';
+import { dirname, resolve } from 'node:path';
+
+import { defineConfig, type Plugin } from 'vite';
 import checker from 'vite-plugin-checker';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+export function htmlInlineSvg(): Plugin {
+  return {
+    name: 'html-inline-svg',
+    transformIndexHtml: {
+      order: 'pre',
+      handler(html, { filename }) {
+        return html.replaceAll(/<img src="(.+\.svg)"(.*?)\/?>/g, (_, src) => {
+          const path = resolve(dirname(filename), src);
+          return readFileSync(path, 'utf-8');
+        });
+      },
+    },
+  };
+}
 
 // Vite configuration
 // https://vitejs.dev/config/
@@ -12,6 +30,8 @@ export default defineConfig(async () => ({
     checker({ typescript: true, overlay: false }),
     // polyfill `node:events` as used by `xmind-model`
     nodePolyfills(),
+    // inline svg files referenced in html files
+    htmlInlineSvg(),
   ],
   // https://github.com/davidmyersdev/vite-plugin-node-polyfills/issues/25#issuecomment-1962228168
   resolve: { alias: { 'node:fs/promises': 'node-stdlib-browser/mock/empty' } },
