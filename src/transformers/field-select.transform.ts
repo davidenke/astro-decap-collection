0ec1c26d0c45f5133dc0dc13b654046cb9eb936d
David Enke <david@enke.dev>
2024-12-17 21:21:16 +0100

    fix: sanitize enum values

--- src/transformers/field-select.transform.ts
+++ src/transformers/field-select.transform.ts
@@ -1,12 +1,13 @@
 import type { CmsFieldBase, CmsFieldSelect } from 'decap-cms-core';
 
+import { escapeString } from '../utils/format.utils.js';
 import type { Transformer } from '../utils/transform.utils.js';
 
 // https://decapcms.org/docs/widgets/#select
 export const transformSelectField: Transformer<CmsFieldBase & CmsFieldSelect> = ({ options }) => {
   const items = options.map(option => (typeof option === 'string' ? option : option.value));
   return {
-    compiled: `z.enum([${items.map(i => `'${i}'`).join(',')}])`,
+    compiled: `z.enum([${items.map(i => `'${escapeString(i)}'`).join(',')}])`,
     dependencies: ['z'],
   };
 };
