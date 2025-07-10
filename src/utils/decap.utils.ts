502a065b0e701f911e1c92ab61f5c5d5a8f9af77
David Enke <>
2025-07-10 22:22:30 +0200

    chore: porting decap config tools
    which could no longer be prevented due to unreliability

--- src/utils/decap.utils.ts
+++ src/utils/decap.utils.ts
@@ -1,5 +1,12 @@
 /* eslint-disable @typescript-eslint/no-empty-function, @typescript-eslint/no-explicit-any, @typescript-eslint/no-dynamic-delete */
-import type { CmsCollection, CmsConfig } from 'decap-cms-core';
+import type {
+  CmsCollection,
+  CmsConfig,
+  CmsField,
+  CmsFieldList,
+  CmsFieldObject,
+} from 'decap-cms-core';
+import * as yaml from 'yaml';
 
 // TODO use the CmsField['widget'] type from decap-cms-core but exclude the generic string somehow
 export type DecapWidgetType =
@@ -19,6 +26,107 @@ export type DecapWidgetType =
   | 'select'
   | 'string'
   | 'text';
+
+// taken from decap config utils -
+// previously imported at runtime from `decap-cms-core/dist/esm/actions/config.js`, which was, quite obviously,
+// not very stable and broke during the last update, thus, this now is ported here from decap-cms-core 3.7.1
+// eslint-disable-next-line @typescript-eslint/no-namespace
+namespace DecapConfigUtils {
+  const WIDGET_KEY_MAP = {
+    dateFormat: 'date_format',
+    timeFormat: 'time_format',
+    pickerUtc: 'picker_utc',
+    editorComponents: 'editor_components',
+    valueType: 'value_type',
+    valueField: 'value_field',
+    searchFields: 'search_fields',
+    displayFields: 'display_fields',
+    optionsLength: 'options_length',
+  } as const;
+
+  function setSnakeCaseConfig(field: CmsField): CmsField {
+    const deprecatedKeys = Object.keys(WIDGET_KEY_MAP).filter(
+      camel => camel in field,
+    ) as (keyof typeof WIDGET_KEY_MAP)[];
+    const snakeValues = deprecatedKeys.map(camel => {
+      const snake = WIDGET_KEY_MAP[camel];
+      console.warn(
+        `Field ${field.name} is using a deprecated configuration '${camel}'. Please use '${snake}'`,
+      );
+      return { [snake]: field[camel as keyof CmsField] };
+    });
+    return Object.assign({}, field, ...snakeValues);
+  }
+
+  function isObjectField(field: object): field is CmsFieldObject {
+    return 'fields' in field;
+  }
+
+  function isFieldList(field: object): field is CmsFieldList {
+    return 'types' in field || 'field' in field;
+  }
+
+  function traverseFieldsJS(
+    fields: CmsField[],
+    updater: (field: CmsField) => CmsField,
+  ): CmsField[] {
+    return fields.map(field => {
+      const newField = updater(field);
+      if (isObjectField(newField)) {
+        return { ...newField, fields: traverseFieldsJS(newField.fields, updater) };
+      } else if (isFieldList(newField) && newField.field) {
+        return { ...newField, field: traverseFieldsJS([newField.field], updater)[0] };
+      } else if (isFieldList(newField) && newField.types) {
+        return { ...newField, types: traverseFieldsJS(newField.types, updater) };
+      }
+      return newField;
+    }) as CmsField[];
+  }
+
+  export function normalizeConfig(config: CmsConfig): CmsConfig {
+    const { collections = [] } = config;
+    const normalizedCollections = collections.map(collection => {
+      const { fields, files } = collection;
+      let normalizedCollection = collection;
+      if (fields) {
+        const normalizedFields = traverseFieldsJS(fields, setSnakeCaseConfig);
+        normalizedCollection = { ...normalizedCollection, fields: normalizedFields };
+      }
+      if (files) {
+        const normalizedFiles = files.map(file => {
+          const normalizedFileFields = traverseFieldsJS(file.fields, setSnakeCaseConfig);
+          return { ...file, fields: normalizedFileFields };
+        });
+        normalizedCollection = { ...normalizedCollection, files: normalizedFiles };
+      }
+      if (normalizedCollection.sortableFields) {
+        const { sortableFields, ...rest } = normalizedCollection;
+        normalizedCollection = { ...rest, sortable_fields: sortableFields };
+        console.warn(
+          `Collection ${collection.name} is using a deprecated configuration 'sortableFields'. Please use 'sortable_fields'`,
+        );
+      }
+      return normalizedCollection;
+    });
+    return { ...config, collections: normalizedCollections };
+  }
+
+  export function parseConfig(data: string): CmsConfig {
+    const config = yaml.parse(data, { maxAliasCount: -1, prettyErrors: true, merge: true });
+    if (
+      typeof window !== 'undefined' &&
+      'CMS_ENV' in window &&
+      typeof window.CMS_ENV === 'string' &&
+      config[window.CMS_ENV]
+    ) {
+      const configKeys = Object.keys(config[window.CMS_ENV]);
+      for (const key of configKeys) {
+        config[key] = config[window.CMS_ENV][key];
+      }
+    }
+    return config;
+  }
+}
 
 export async function loadDecapConfig(ymlPath: string): Promise<CmsConfig | undefined> {
   const { existsSync } = await import('node:fs');
@@ -54,11 +162,11 @@ export async function parseConfig(ymlData: string): Promise<CmsConfig | undefine
   }
 
   // load the original tooling...
-  let decapConfig = await import('decap-cms-core/dist/esm/actions/config.js');
-  if ('default' in decapConfig) decapConfig = decapConfig.default as any;
+  // let decapConfig = await import('decap-cms-core/dist/esm/actions/config.js');
+  // if ('default' in decapConfig) decapConfig = decapConfig.default as any;
 
   try {
-    return decapConfig.normalizeConfig(decapConfig.parseConfig(ymlData) ?? {});
+    return DecapConfigUtils.normalizeConfig(DecapConfigUtils.parseConfig(ymlData));
   } catch (error) {
     console.warn('Problems parsing Decap CMS config:', error);
     return undefined;
